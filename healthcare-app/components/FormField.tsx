7a45670d1d9981b1e5a43b5ea6558947331eb47c
detective-sokka <saidutt1997@gmail.com>
2024-11-12 14:24:37 -0500

    Change FormField TextInput text color to default(black).
    * Remove auto capitalisation of TextInputs.

--- healthcare-app/components/FormField.tsx
+++ healthcare-app/components/FormField.tsx
@@ -27,8 +27,9 @@ const FormField: React.FC<FormFieldProps> = ({
 
       <View className="w-full h-16 px-4 bg-white rounded-2xl border-2 border-black-200 focus:border-secondary flex flex-row items-center">
         <TextInput
-          className="flex-1 text-white font-psemibold text-base"
+          className="flex-1 font-psemibold text-base"
           value={value}
+          autoCapitalize="none"
           placeholder={placeholder}
           placeholderTextColor="#7B7B8B"
           onChangeText={handleChangeText}
