95e17d0c4b6b976060dc759757a49e2be9b6f02f
ManishCP <manishcp77@gmail.com>
2024-12-11 17:29:08 -0500

    Notification service implemented

---
+++ healthcare-app/constants/thresholds.ts
@@ -0,0 +1,24 @@
+// constants/thresholds.ts
+export const THRESHOLDS = {
+    airQuality: {
+      pm10: 50,
+      pm2_5: 25,
+      aqi: 100
+    },
+    pollen: {
+      Tree: 'High',
+      Grass: 'High',
+      Ragweed: 'High',
+      Mold: 'High'
+    },
+    covid: {
+      testPositivityRatio: 0.1,
+      vaccinationRatio: 0.5
+    },
+    waterQuality: {
+      pH: 8.5,
+      lead: 0.015,
+      eColi: 100
+    }
+  };
+  
